e7570d4b2431fbdbbe9f67eb32ffdb6782ce66e6
vinod4362 <vetukurivinod@gmail.com>
2024-11-10 23:13:00 +0100

    reapplying text cases

--- tests/test_character_frequency.py
+++ tests/test_character_frequency.py
@@ -7,4 +7,4 @@ def test_character_frequency():
     assert character_frequency("aabbcc") == {"a": 2, "b": 2, "c": 2}
     assert character_frequency("abcABC") == {"a": 1, "b": 1, "c": 1, "A": 1, "B": 1, "C": 1}
     assert character_frequency("cdeCDE") == {"a": 1, "b": 1, "c": 1, "A": 1, "B": 1, "C": 1}
-    assert character_frequency("bye") == {"h": 1, "e": 1, "l": 2, "o": 1}
+    assert character_frequency("vinod") == {"h": 1, "e": 1, "l": 2, "o": 1}
