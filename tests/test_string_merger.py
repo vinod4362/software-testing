e5f04a1e782e3627981993beb3dc7c4a4d29a4bc
vinod4362 <vetukurivinod@gmail.com>
2024-11-10 23:28:04 +0100

    new test case added in the test string file

--- tests/test_string_merger.py
+++ tests/test_string_merger.py
@@ -5,3 +5,4 @@ from app.word_counter import count_words
 def test_merge_and_count():
     merged_string = merge_strings("abc", "123")
     assert count_words(merged_string) == 1  # "a1b2c3" should be counted as a single word
+    assert count_words(merged_string) == 1 
